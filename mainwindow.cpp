2d14a52874db37a1284e5e75fb1ad60dbdd44a27
zheleznyakov <zhelezny.andrey@gmail.com>
2019-03-17 15:43:21 +0300

    Добавлены комментарии

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,21 +8,27 @@ ui(new Ui::MainWindow)
 {
 ui->setupUi(this);
 
+//объявляем новую модель данных
 QStringListModel *model= new QStringListModel(this);
-
+//добавляем данные в созданную модель
 model->setStringList(QStringList()<<"Элемент1"<<"Элемент2"<<"Элемент3");
+//каждому элементу представления присваиваем созданную модель данных
 ui->treeView->setModel(model);
 ui->listView->setModel(model);
 ui->tableView->setModel(model);
 
+/* следующий код создаёт общую модель для выделения элементов
+ * если мы выделим какой - либо элемент в одном из представлений,
+ * он автоматически выделится в остальных представлениях
+*/
 QItemSelectionModel *selection = new QItemSelectionModel(model);
 ui->listView->setSelectionModel(selection);
 ui->treeView->setSelectionModel(selection);
 ui->tableView->setSelectionModel(selection);
 
-QModelIndex index = model->index(1,0);//программно выбираем вторую строку
-
-selection->select(index,QItemSelectionModel::Select);
+// программный выбор одного из элементов модели(в нашем случае вторая строка)
+QModelIndex index = model->index(1,0); //находим индекс второй строки
+selection->select(index,QItemSelectionModel::Select);//выделяем соответствующую строку
 }
 
 MainWindow::~MainWindow()
